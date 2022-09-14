c135f67d753eb181e3aec0443f52cb3275075411
dmlgus1922 <dmlgus1922@naver.com>
2022-09-14 14:01:06 +0900

    momentum

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ function handleTitleClick() {
 }
 
 function handleMouseEnter() {
-    console.log("mouse is here!");
+    title.innerText = 'Mouse is here';
+}
+
+function handleMouseLeave() {
+    title.innerText = 'Mouse is gone';
 }
 
 title.addEventListener("click", handleTitleClick);
 title.addEventListener("mouseenter", handleMouseEnter);
-
-ㅁㄴ
+title.addEventListener("mouseleave", handleMouseLeave);
