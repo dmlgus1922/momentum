bf12b7564f4033e0e945fb782071dbac2245a854
dmlgus1922 <dmlgus1922@naver.com>
2022-09-14 13:42:35 +0900

    test

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    title.style.color ="blue";
+}
+
+function handleMouseEnter() {
+    console.log("mouse is here!");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+
+ㅁㄴ
