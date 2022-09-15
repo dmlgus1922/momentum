7667f1e81edb1f5dd215479f5e2d0f9ee56ffd1e
dmlgus1922 <dmlgus1922@naver.com>
2022-09-16 08:51:41 +0900

    20220915

--- web/app.js
+++ web/app.js
@@ -1,15 +1,35 @@
 // const loginForm = document.getElementById("login-form");
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
-const loginForm = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    // const username = loginInput.value;
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
+    
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-onLoginSubmit
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreeting(savedUsername);
+}
+console.log(savedUsername);
