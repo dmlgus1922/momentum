efbd5ca34223fe329943dbacfccd141d63c1e304
dmlgus1922 <dmlgus1922@naver.com>
2022-09-21 21:49:41 +0900

    20220921

--- web/js/greetings.js
+++ web/js/greetings.js
@@ -6,18 +6,19 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username"
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreeting(username);
+    paintGreeting();
     
 }
 
-function paintGreeting(username) {
+function paintGreeting() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
@@ -30,6 +31,6 @@ if (savedUsername == null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     // show the greetings
-    paintGreeting(savedUsername);
+    paintGreeting();
 }
 console.log(savedUsername);
