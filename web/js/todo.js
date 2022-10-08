7e5bc35ea9d710f3fa63fc97a3c7ccfc068d74de
dmlgus1922 <dmlgus1922@naver.com>
2022-10-08 17:50:28 +0900

    221008

--- web/js/todo.js
+++ web/js/todo.js
@@ -2,11 +2,26 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem('todos', toDos);
+}
+
+function deleteToDo(e) { // 어떤 버튼이 클릭됐는지 알기 위해 event를 확인
+    const li = e.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement('li');
     const span = document.createElement('span');
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement('button');
+    button.innerText = "X";
+    button.addEventListener('click', deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -14,7 +29,9 @@ function handleTodoSubmit(e) {
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = '';
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleTodoSubmit);
